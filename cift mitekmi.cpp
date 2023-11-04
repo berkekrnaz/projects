f32fd93543a9cd3fca3fe0f40286e863b61e70de
berkekrnaz <147308734+berkekrnaz@users.noreply.github.com>
2023-11-04 15:14:04 +0300

    c++ projeler

---
+++ cift mitekmi.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	
+int no1;
+printf("Sayiyi gir.");
+scanf("%d",&no1);
+
+no1%2==0?printf("Cift"):printf("Tek");	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ snbasmk.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+int main () {
+	
+int n1;
+printf("Sayiyi gir.");
+scanf("%d",&n1);
+switch(n1%10)
+{
+	
+case 0:
+ printf("Son basamak 0");
+ break;	
+case 1:
+ printf("Son basamak 1");
+ break;	
+case 2:
+ printf("Son basamak 2");
+ break;	
+ case 3:
+ printf("Son basamak 3");
+ break;	
+ case 4:
+ printf("Son basamak 4");
+ break;	
+ case 5:
+ printf("Son basamak 5");
+ break;	
+	case 6:
+ printf("Son basamak 6");
+ break;	
+ case 7:
+ printf("Son basamak 7");
+ break;	
+ case 8:
+ printf("Son basamak 8");
+ break;	
+ case 9:
+ printf("Son basamak 9");
+ break;	
+ default:
+ 	printf("Hata");	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+}
